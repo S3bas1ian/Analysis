12be1055263bca3407654b644ff26c83d68935d8
S3bas1ian <sebastian@docschroeder.net>
2023-06-14 11:37:53 +0200

    first cluste sizes

--- ana_orthogonal/ausprobieren.cc
+++ ana_orthogonal/ausprobieren.cc
@@ -1,12 +1,35 @@
-void ausprobieren(){
+#include <TFile.h>
+#include <TTree.h>
 
-    TFile* file = new TFile("mess_1/output_t0.root", "read");
-    TTree* hits = (TTree*) file->Get("hits");
+void ausprobieren()
+{
 
-    char name_hitscoll [128];
+    //File which will be read
+    TFile *file = new TFile("mess_1/output_t0.root", "read");
+    TTree *hits = (TTree *)file->Get("hits");
+
+    //File, where I write the cluster sizes out
+    TFile *outputFile = newTFile("cluster_size.root", "recreate");
+    TTree *outputTree = new TTree("cluster_size", "cluster_size");
+
+    int event_size;
+
+    outputTree->Branch("event_size", &event_size, "event_size/I");
+
+    char name_hitscoll[128];
+    int event_number;
 
     hits->SetBranchAddress("Hit_Name", &name_hitscoll);
+    hits->SetBranchAddress("event", &event_number);
 
-    hits->GetEntry(0);
-    cout << name_hitscoll << endl;
+    while (int i < hits->GetEntries())
+    {
+        hits->GetEntry(i);
+        event_size = hits->Scan("","events=" + str(event_number))
+        outputTree->Fill();
+        i += event_size;
+    }
+
+    outputFile->Write();
+    outputFile->Close();
 }
