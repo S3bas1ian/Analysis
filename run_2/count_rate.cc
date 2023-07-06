57c3a5dda106ed4ccae475cc77e30f90d844fef6
Sebastian <80919128+S3bas1ian@users.noreply.github.com>
2023-07-06 17:19:31 +0200

    new analysis script
    calculates for an specific strip the time between two events

---
+++ run_2/count_rate.cc
@@ -0,0 +1,58 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <string.h>
+#include <vector.h>
+
+
+void count_rate(){
+
+	int det = 0;
+	int strip = 123;
+	double e_min = 100000; //unit eV
+	
+	double psPerEvent = 1/100000000; //1s/10^8ions in this one second
+	vector<double> vec_time;
+
+	TFile *file = new TFile("data/final_output/final_output.root", "read");
+    	TTree *hits = (TTree *)file->Get("hits");
+
+	char particle_name[128];
+    	int event_number;
+    	int det_id;
+	int strip_id;
+    	double edep;
+	double time;
+
+	
+    	hits->SetBranchAddress("name", &particle_name);
+    	hits->SetBranchAddress("event", &event_number);
+    	hits->SetBranchAddress("Det_ID", &det_id);
+	hits->SetBranchAddress("Strip_ID", &strip_id);
+    	hits->SetBranchAddress("edep", &edep);
+	hits->SetBranchAddress("time", &time);
+
+
+    	// preload stuff to speed things up
+    	hits->LoadBaskets();
+
+    	//int i = 0;
+    	int size = hits->GetEntries();
+	
+	for(int i=0; i<size; i++){
+		hits->GetEntry(i);
+
+		if(det_id==det && strip_id==strip){
+			t = psPerEvent*event_number + time;
+			vec_time.insert(t);
+		}
+
+	}
+
+
+	std::sort(vec_time.begin(), vec_time.end());
+	auto g = new TGraph(vec_time.size(), vec_time);
+	g->SetTitle("Time between hits in specific strip");
+	g->Draw();
+	g->SaveAs("time_elapsed.png");
+
+}
