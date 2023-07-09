dba80d5d8e5c9a2aaa80c93e20e92a1a16a1dbbb
Sebastian <sebastian@docschroeder.net>
2023-07-09 14:47:09 +0200

    comments in this garbage of code

--- run_2/count_rate.cc
+++ run_2/count_rate.cc
@@ -17,6 +17,7 @@ void count_rate()
 	TFile *file = new TFile("data/final_output/output3.root", "read");
 	TTree *hits = (TTree *)file->Get("hits");
 
+	//variables that are connected to the root file
 	char particle_name[128];
 	int event_number;
 	int det_id;
@@ -24,20 +25,25 @@ void count_rate()
 	double edep;
 	double time;
 
+	//connect variables with the branch from the root file
 	hits->SetBranchAddress("name", &particle_name);
 	hits->SetBranchAddress("event", &event_number);
 	hits->SetBranchAddress("Det_ID", &det_id);
 	hits->SetBranchAddress("Strip_ID", &strip_id);
 	hits->SetBranchAddress("edep", &edep);
 	hits->SetBranchAddress("time", &time);
-	hits->LoadBaskets();
 
 	// preload stuff to speed things up
+	hits->LoadBaskets();
+
+	//iterate through the wanted detectors and strips
 	for (int d : det)
 	{
 		for (int s : strip)
 		{
 			double* o;
+
+			//calculate for each det/strip combnination the mean and stdv
 			o = get_Mean_and_Stdv(d, s);
 			cout << "------------ detector: " << d << "and strips: " << s << "------------"  << endl;
 			cout << "average [ps]: " << *o << endl;
@@ -51,28 +57,33 @@ void count_rate()
 	// c1->SaveAs("time_elapsed.png");
 }
 
-double* get_Mean_and_Stdv(int det_id, int strip_id)
+double* get_Mean_and_Stdv(int det_id, int strip_id)	//should return mean and stdv
 {
 	
 
 	int size = this.hits->GetEntries();
 
+	//iterate through the whole root file
 	for (int i = 0; i < size; i++)
 	{
 		this.hits->GetEntry(i);
 
+		//if the current entry is on the specific strip
 		if (this.det_id == det_id && this.strip_id == strip_id)
 		{
+			//then add the time point to the vector
 			double t = this.psPerEvent * this.event_number + this.time;
 			this.vec_time_point.push_back(t);
 		}
 	}
 
+	//sort the time point vector
 	std::sort(this.vec_time_point.begin(), this.vec_time_point.end());
 
 	vector<double> delta_time;
 	//auto h = new TH1D("hist", "delta time", 100, 0, 2);
 
+	//calculate the time between two events
 	for (int i = 0; i < vec_time_point.size() - 1; i++)
 	{
 		double t = this.vec_time_point[i + 1] - this.vec_time_point[i];
@@ -80,6 +91,7 @@ double* get_Mean_and_Stdv(int det_id, int strip_id)
 		//h->Fill(t);
 	}
 
+	//calculate mean and stdv and return it
 	double sum = std::accumulate(std::begin(delta_time), std::end(delta_time), 0.0);
 	double m = sum / delta_time.size();
 
@@ -89,5 +101,5 @@ double* get_Mean_and_Stdv(int det_id, int strip_id)
 
 	double stdev = sqrt(accum / (delta_time.size() - 1));
 	double out[] = {m, stdev};
-	return out;
+	return *out;
 }
