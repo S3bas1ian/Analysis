f4f0580313da36eba5d72fbc479e258ba8a4ef61
S3bas1ian <sebastian@docschroeder.net>
2023-08-16 15:30:34 +0200

    completely new edge_hit

--- run_2/edge_hit.cc
+++ run_2/edge_hit.cc
@@ -2,7 +2,7 @@
 #include <TTree.h>
 #include <string.h>
 
-void edge_hit(std::string path, std::string particle, int s)
+void edge_hit(std::string path, std::string particle)
 {
 
     // constants
@@ -12,9 +12,9 @@ void edge_hit(std::string path, std::string particle, int s)
     TTree *hits = (TTree *)file->Get("hits");
 
     auto h1_1d = new TH1I("edges_det_0", "edges detector 0; N_{Counts}; #", 2, 0, 0);
-    auto h2_1d = new TH1I("edges_det_1", "edges detector 1; N_{Counts}; #", 2, 0, 1);
-    auto h3_1d = new TH1I("edges_det_2", "edges detector 2; N_{Counts}; #", 2, 0, 1);
-    auto h4_1d = new TH1I("edges_det_3", "edges detector 3; N_{Counts}; #", 2, 0, 1);
+    // auto h2_1d = new TH1I("edges_det_1", "edges detector 1; N_{Counts}; #", 2, 0, 1);
+    // auto h3_1d = new TH1I("edges_det_2", "edges detector 2; N_{Counts}; #", 2, 0, 1);
+    // auto h4_1d = new TH1I("edges_det_3", "edges detector 3; N_{Counts}; #", 2, 0, 1);
 
     char particle_name[128];
     Int_t event_number;
@@ -68,10 +68,15 @@ void edge_hit(std::string path, std::string particle, int s)
         back_size_det_3 = 0;
         back_size_det_4 = 0;
         event_size = 0;
+        Int_t last_strip_id_1;
+        Int_t last_strip_id_2;
+        Int_t last_strip_id_3;
+        Int_t last_strip_id_4;
 
         hits->GetEntry(i);
         int currEvent = event_number;
         bool sameEvent = true;
+        std::vector<int> strips;
 
         while (sameEvent && i < size) // loop through one full event, increase i while doing so
         {
@@ -83,47 +88,36 @@ void edge_hit(std::string path, std::string particle, int s)
 
                 if (edep > energy_min && std::string(particle_name).compare(particle) == 0) // only count event if energy is deposited  && std::string(particle_name).compare("e+")==0
                 {
-                    // count the event
-                    event_size += 1;
-
-                    if (det_id == 0 && (strip_id < s || strip_id> 1023-s))
+                    if (det_id == 0)
                     {
-                        event_size_det_1 += 1;
                         front_size_det_1 += 1;
                     }
-                    else if (det_id == 1 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 1)
                     {
-                        event_size_det_1 += 1;
                         back_size_det_1 += 1;
                     }
-                    else if (det_id == 2 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 2)
                     {
-                        event_size_det_2 += 1;
                         front_size_det_2 += 1;
                     }
-                    else if (det_id == 3 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 3)
                     {
-                        event_size_det_2 += 1;
                         back_size_det_2 += 1;
                     }
-                    else if (det_id == 4 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 4)
                     {
-                        event_size_det_3 += 1;
                         front_size_det_3 += 1;
                     }
-                    else if (det_id == 5 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 5)
                     {
-                        event_size_det_3 += 1;
                         back_size_det_3 += 1;
                     }
-                    else if (det_id == 6 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 6)
                     {
-                        event_size_det_4 += 1;
                         front_size_det_4 += 1;
                     }
-                    else if (det_id == 7 && (strip_id < s || strip_id> 1023-s))
+                    else if (det_id == 7)
                     {
-                        event_size_det_4 += 1;
                         back_size_det_4 += 1;
                     }
                     i += 1;
@@ -137,57 +131,32 @@ void edge_hit(std::string path, std::string particle, int s)
             {
                 sameEvent = false;
             }
+
+            if((det_id==0 && back_size_det_1 == 0) || (det_id==1 && front_size_det_1 == 0)){
+                strips.push_back(strip_id);
+            }else if (strips.size()>0){
+                strips.resize(0);
+            }
+
         }
 
         // check if "one-hitter"
-        if (event_size > 0)
+        if ((det_id==0 && back_size_det_1 == 0) || (det_id==1 && front_size_det_1 == 0))
         {
-            if ((front_size_det_1 == 0 && back_size_det_1 > 0) || (front_size_det_1 > 0 && back_size_det_1 == 0))
-            {
-                h1_1d->Fill("edge", 1);
-            }
-            else if (front_size_det_1 > 0 && back_size_det_1 > 0)
-            {
-                h1_1d->Fill("no edge", 1);
-            }
-
-            if ((front_size_det_2 == 0 && back_size_det_2 > 0) || (front_size_det_2 > 0 && back_size_det_2 == 0))
-            {
-                h2_1d->Fill("edge", 1);
-            }
-            else if (front_size_det_2 > 0 && back_size_det_2 > 0)
-            {
-                h2_1d->Fill("no edge", 1);
-            }
-
-            if ((front_size_det_3 == 0 && back_size_det_3 > 0) || (front_size_det_3 > 0 && back_size_det_3 == 0))
-            {
-                h3_1d->Fill("edge", 1);
-            }
-            else if (front_size_det_3 > 0 && back_size_det_3 > 0)
-            {
-                h3_1d->Fill("no edge", 1);
-            }
-
-            if ((front_size_det_4 == 0 && back_size_det_4 > 0) || (front_size_det_4 > 0 && back_size_det_4 == 0))
-            {
-                h4_1d->Fill("edge", 1);
-            }
-            else if (front_size_det_4 > 0 && back_size_det_4 > 0)
-            {
-                h4_1d->Fill("no edge", 1);
+            for(int s:strips){
+                h1_1d->Fill(s);
             }
         }
     }
 
-    auto c2 = new TCanvas((std::string("edge_hits_particle==") + particle).c_str(), (std::string("edge hits for ") + particle + std::string(" (e_min= 100keV)")).c_str());
-    c2->Divide(2, 2);
-    c2->cd(1);
-    h3_1d->Draw();
-    c2->cd(2);
-    h4_1d->Draw();
-    c2->cd(3);
+    auto c2 = new TCanvas((std::string("edge_hits_particle==") + particle).c_str(), (std::string("edge hits for ") + particle + std::string(" (e_min= 100keV, strips=") + std::string(s)).c_str());
+    // c2->Divide(2, 2);
+    // c2->cd(1);
+    // h3_1d->Draw();
+    // c2->cd(2);
+    // h4_1d->Draw();
+    // c2->cd(3);
     h1_1d->Draw();
-    c2->cd(4);
-    h2_1d->Draw();
+    // c2->cd(4);
+    // h2_1d->Draw();
 }
