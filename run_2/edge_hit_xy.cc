01162d8d35f09b2c0c6bd60df4f5384142f620ee
S3bas1ian <sebastian@docschroeder.net>
2023-08-18 11:12:03 +0200

    naming axis

--- run_2/edge_hit_xy.cc
+++ run_2/edge_hit_xy.cc
@@ -11,7 +11,7 @@ void edge_hit_xy(std::string path, std::string particle)
     TFile *file = new TFile(path.c_str(), "read");
     TTree *hits = (TTree *)file->Get("hits");
 
-    auto h1_2d = new TH2D("edges_det_0", "edges detector 0;x; y", 1000, 90, 190, 1000, -50, 50);
+    auto h1_2d = new TH2D("edges_det_0", "edges detector 0; x; y", 1000, 90, 190, 1000, -50, 50);
     auto h2_2d = new TH2D("edges_det_1", "edges detector 1; x; y", 1000, 90, 190, 1000, -50, 50);
     auto h3_2d = new TH2D("edges_det_2", "edges detector 2; x; y", 1000, 90, 190, 1000, -50, 50);
     auto h4_2d = new TH2D("edges_det_3", "edges detector 3; x; y", 1000, 90, 190, 1000, -50, 50);
