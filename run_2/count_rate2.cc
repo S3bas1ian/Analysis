54e21f316af63e784ab42d1cdba0a4cbca12879b
S3bas1ian <sebastian@docschroeder.net>
2023-09-06 13:53:23 +0200

    errors

--- run_2/count_rate2.cc
+++ run_2/count_rate2.cc
@@ -194,7 +194,7 @@ void count_rate2(std::string path, std::string particle, std::string draw_opt, b
         Double_t x[3] = { 1.0, 2.0 , 3.0};
         Double_t y[3] = {3.1, 4.5, 6.6};
         Double_t ey[3] = {0.1, 0.2, 0.3};
-        auto test = new TGraph(3, x, y, ey);
+        auto test = new TGraphErrors(3, x, y, ey);
         test->SetName("test");
 
         for (int i = 0; i < 4; i++)
