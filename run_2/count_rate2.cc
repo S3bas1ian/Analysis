221849c82f4858764cf06b0dbc3a2ab329a8bc4a
S3bas1ian <sebastian@docschroeder.net>
2023-09-06 14:12:45 +0200

    errors

--- run_2/count_rate2.cc
+++ run_2/count_rate2.cc
@@ -191,10 +191,10 @@ void count_rate2(std::string path, std::string particle, std::string draw_opt, b
         }
 
         std::vector<TCanvas *> canvases2;
-        std::vector<Double_t> x[3] = { 1.0, 2.0 , 3.0};
-        std::vector<Double_t> y[3] = {3.1, 4.5, 6.6};
-        std::vector<Double_t> ey[3] = {0.1, 0.2, 0.3};
-        auto test = new TGraphErrors(x, y,0 , ey0);
+        std::vector<Double_t> x3 = {1.0, 2.0 , 3.0};
+        std::vector<Double_t> y3 = {3.1, 4.5, 6.6};
+        std::vector<Double_t> ey3 = {0.1, 0.2, 0.3};
+        auto test = new TGraphErrors(x, y, 0, ey);
         test->SetName("test");
 
         for (int i = 0; i < 4; i++)
