ffc765496bfe83fcfc0c2159368a67dce929e7a2
S3bas1ian <sebastian@docschroeder.net>
2023-09-08 11:09:02 +0200

    lighter color

--- run_2/count_rate2.cc
+++ run_2/count_rate2.cc
@@ -217,7 +217,7 @@ void count_rate2(std::string path, std::string particle, std::string draw_opt, b
             gr_errors.push_back(new TGraphErrors(strip.size(), &strip[0], &mean[0], 0, &stdv[0]));
             gr_errors[d]->SetName("#Delta t per strip");
             gr_errors[d]->SetMarkerStyle(4);
-            gr_errors[d]->SetMarkerColorAlpha(kAzure-4, 0.7);
+            gr_errors[d]->SetMarkerColorAlpha(kAzure-2, 0.7);
             gr_errors[d]->SetMarkerSize(1.2);
             gr_errors[d]->SetLineColorAlpha(kAzure-4, 0.1);
             gr_errors[d]->GetYaxis()->SetLabelColor(kAzure-4, 1);
