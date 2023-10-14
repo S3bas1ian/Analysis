4d042e353de6f89f1d368811a07ee53c314df99d
S3bas1ian <sebastian@docschroeder.net>
2023-10-14 15:30:50 +0200

    fixed it

--- run_2/count_rate2.cc
+++ run_2/count_rate2.cc
@@ -311,7 +311,7 @@ void count_rate2(std::string path, std::string particle, Double_t e, std::string
                 }
                     strip.push_back(static_cast<double>(s));
                     mean.push_back(1e9 / stats[d][s][0]);
-                    stdv.push_back(1e9 / stats[d][s][1]);
+                    stdv.push_back(0);  //1e9 / stats[d][s][1]
                     hits.push_back(timestamps[d][s].size());
                 }
             }
@@ -361,7 +361,7 @@ void count_rate2(std::string path, std::string particle, Double_t e, std::string
             graphs[i * 2]->Draw("ALY+");
 
             legends.push_back(new TLegend(0.45, 0.77, 0.7, 0.9));
-            legends[i]->AddEntry(gr_errors[i * 2], "count rate per strip with 1 #sigma", "lep");
+            legends[i]->AddEntry(gr_errors[i * 2], "count rate per strip ", "lep"); //with 1 #sigma"
             legends[i]->AddEntry(graphs[i * 2], "hits per strip", "l");
             legends[i]->Draw();
 
