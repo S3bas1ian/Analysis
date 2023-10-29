2d15421a84cf6cf38aaa54db62bfd33a3e41d50d
S3bas1ian <sebastian@docschroeder.net>
2023-10-29 18:45:38 +0100

    improving visibility

--- run_2/edge_hit_strip.cc
+++ run_2/edge_hit_strip.cc
@@ -187,11 +187,11 @@ void edge_hit_strip(std::string path, std::string particle, Double_t energy_min)
     gStyle->SetTitleFont(43, "xyz");
     gStyle->SetLabelFont(43, "xyz");
     gStyle->SetTickLength(0.04, "xyz");
-    gStyle->SetLineWidth(4);
+    gStyle->SetLineWidth(5);
     gStyle->SetOptStat(0);
     gStyle->SetPadBottomMargin(0.17);
     gStyle->SetPadLeftMargin(0.13);
-    gStyle->SetPadRightMargin(0.03);
+    gStyle->SetPadRightMargin(0.02);
 
     //plotting
     auto c2 = new TCanvas((std::string("edge_hitsStrip_particle_") + particle).c_str(), (std::string("edge hits (strip) for ") + particle + std::string(" (e_min= 100keV)")).c_str());
