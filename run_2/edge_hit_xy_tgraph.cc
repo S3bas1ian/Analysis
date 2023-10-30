43c051a15f361fb40e88a89c82c2324107ed0337
S3bas1ian <sebastian@docschroeder.net>
2023-10-30 15:43:11 +0100

    reworking legibility

--- run_2/edge_hit_xy_tgraph.cc
+++ run_2/edge_hit_xy_tgraph.cc
@@ -305,7 +305,7 @@ void edge_hit_xy_tgraph(std::string path, std::string particle, Double_t energy_
     gStyle->SetPadBottomMargin(0.15);
     gStyle->SetPadLeftMargin(0.17);
     gStyle->SetPadRightMargin(0.02);
-    gStyle->SetPadTopMargin(0.03);
+    gStyle->SetPadTopMargin(0.06);
     gStyle->SetLabelOffset(0.015, "y");
 
     auto graph1_phantom = new TGraph(x1_phantom.size(), x1_phantom.data(), y1_phantom.data());
